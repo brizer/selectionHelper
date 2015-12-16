49d568b6a93e29d3c8fe7a8a3ccf94ef0c298f7b
brizer <2281858473@qq.com>
2015-12-16 16:49:29 +0800

    popup页面select操作完成

--- js/background.js
+++ js/background.js
@@ -1,8 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
 	window.open("http://www.baidu.com/baidu?word="+"helloworld","_blank");
 });
@@ -12,7 +8,8 @@ function test(){
 	chrome.tabs.query({active:true,currentWindow:true},function(tabs){
 		var tab = tabs[0];
 		var url = tab.url;
-		alert(url);
+		// alert(url);
 	});
+	localStorage.setItem("userSelection","123");
 	
 }
--- js/popup.js
+++ js/popup.js
@@ -1,2 +1,9 @@
 var bg = chrome.extension.getBackgroundPage();
 bg.test();
+
+var selection = document.getElementById("search_name");
+var selected = 0;
+selection.addEventListener("change",function(){
+	var selected = selection.options[selection.selectedIndex].value;
+	console.log(selected);
+},false);
